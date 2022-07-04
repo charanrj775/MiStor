6c48f9d45bf49e8584226dea89046420bde266e2
charanrj775 <97695073+charanrj775@users.noreply.github.com>
2022-07-04 14:40:12 +0530

    miclone

--- src/App.js
+++ src/App.js
@@ -7,8 +7,7 @@ import data from "./data/data.json"
 import Offers from "./components/Offers.js"
 import Heading  from "./components/Heading.js"
 import StarProduct from "./components/StarProduct.js"
-//  import HotAccessoriesMenu  from "./components/HotAccessoriesMenu.js"
-//  import HotAccessories from "./components/HotAccessories.js"
+
 import ProductReviews from "./components/ProductReviews.js"
 import Videos from "./components/Videos.js"
 import Banner from "./components/Banner.js"
@@ -31,30 +30,6 @@ function App() {
        <Offers offer={data.offer} />
        <Heading text="STAR PRODUCTS"/>
        <StarProduct starProduct={data.starProduct} />
-        {/* <Heading text="HOT ACCESSORIES" />
-        <HotAccessoriesMenu />
-
-       <Route exact path="/music">
-       <HotAccessories music={data.hotAccessories.music} musicCover={data.hotAccessoriesCover.music}  />
-       </Route>
-
-       <Route exact path="/smartDevice">
-       <HotAccessories smartDevice={data.hotAccessories.smartDevice} smartDeviceCover={data.hotAccessoriesCover.smartDevice}  />
-       </Route>
-
-       <Route exact path="/home">
-       <HotAccessories home={data.hotAccessories.home} homeCover={data.hotAccessoriesCover.home}  />
-       </Route>
-
-       <Route exact path="/lifestyle">
-       <HotAccessories lifeStyle={data.hotAccessories.lifeStyle} lifeStyleCover={data.hotAccessoriesCover.lifeStyle}  />
-       </Route>
-
-
-       <Route exact path="/mobileAccessories">
-       <HotAccessories mobileAccessories={data.hotAccessories.mobileAccessories} mobileAccessoriesCover={data.hotAccessoriesCover.mobileAccessories}  />
-       </Route>   */}
-
        <Heading text="PRODUCT REVIEWS"/>
 
        <ProductReviews productReviews={data.productReviews}/>
